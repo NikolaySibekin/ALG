317e4eb45e5a3933930dd5f53b2b0fb2d812ada9
NikolaySibekin <nik-sibekin@yandex.ru>
2019-01-17 22:06:08 +0300

    dz_4

---
+++ task_2.py
@@ -0,0 +1,19 @@
+import cProfile
+
+def get_len(array):
+    return len(array)
+
+def get_sum(array):
+    sum_ = 0
+    for item in array:
+        sum_ += item
+    return sum_
+
+def main():
+    lst = [i for i in range(10000)]
+    len_ = get_len(lst)
+    sum_ = get_sum(lst)
+
+cProfile.run('main()')
+
+
